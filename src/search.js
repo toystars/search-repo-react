847a5eb41b20a9024961c2c19bcaf7a05f848e07
Excellence-daniel <43383067+Excellence-daniel@users.noreply.github.com>
2018-11-29 11:01:41 +0100

    Update search.js
    import all libraries and components to be used in this app

--- src/search.js
+++ src/search.js
@@ -1,1 +1,7 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import axios from "axios";
+import "./search.css"
 
+import {AsyncStorage} from "AsyncStorage"
+import DisplayTable from './components/displayTable';
